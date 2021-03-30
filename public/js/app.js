c62e757d1cdfd1ac1d169a1700cc3df59cbf4f43
Ryan Wong <ryan@megaphonetv.com>
2021-03-30 08:56:48 -0400

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -18,7 +18,7 @@ fetch('http://puzzle.mead.io/puzzle').then(res => {
 })
 
 function retrieveWeather(value) {
-    fetch(`http://localhost:3000/weather?address=${value}`).then(res => {
+    fetch(`/weather?address=${value}`).then(res => {
         res.json().then(data => {
             if (data.error) {
                 console.log(data.error);
