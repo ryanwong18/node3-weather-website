1d67cb7ecefb8e8e9aa37f6bde77e214d3baae21
Ryan Wong <ryan@megaphonetv.com>
2021-03-30 09:10:21 -0400

    added new data to forecast.js

--- utils/forecast.js
+++ utils/forecast.js
@@ -4,13 +4,14 @@ const forecast = (longitude, latitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=2bc1b5ae2034e5f9cea8f8f2e9ffe26b&query=${latitude},${longitude}&units=f`;
     
     request({ url, json: true }, (error, response) => {
-        const { temperature, feelslike } = response.body.current;
+        console.log(response.body);
+        const { temperature, feelslike, observation_time } = response.body.current;
         if (error) {
             callback('Unable to connect to weather service!');
         } else if (response.body.error) {
             callback('Unable to find location!');
         } else {
-            callback(undefined, `It is currently ${temperature}. It feels like ${feelslike}`);
+            callback(undefined, `It is currently ${temperature}. It feels like ${feelslike}. The observation time is ${observation_time}`);
         }
     })
 }
